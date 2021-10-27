ed424516bacf454ee09512fa65705ed6026cfe6b
Joren Rothman <rothmanjoren@gmail.com>
2021-10-27 18:28:32 +0200

    Added env example file

--- server/src/services/database.ts
+++ server/src/services/database.ts
@@ -4,16 +4,15 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-const { DB_HOST, DB_PORT, DB_NAME, DB_USER_NAME, DB_USER_PASSWORD } =
-    process.env
+const { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_USER_PASS } = process.env
 
 const knex = Knex({
     client: "mysql",
     connection: {
         host: DB_HOST ? DB_HOST : "127.0.0.1",
         port: DB_PORT ? Number.parseInt(DB_PORT) : 3306,
-        user: DB_USER_NAME,
-        password: DB_USER_PASSWORD,
+        user: DB_USER,
+        password: DB_USER_PASS,
         database: DB_NAME,
     },
     debug: true,
